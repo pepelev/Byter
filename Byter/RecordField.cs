b5725d14b606b21b5be0bb8380d84f795f86a251
pepelev <kran440@gmail.com>
2023-04-21 11:45:31 +0500

    Fixes RecordField.ToString()

--- Byter/RecordField.cs
+++ Byter/RecordField.cs
@@ -2,5 +2,5 @@ namespace Byter;
 
 public sealed record RecordField(string Format, string Name)
 {
-    public override string ToString() => $"{Format}, {Name}";
+    public override string ToString() => $"{Format} {Name}";
 }
